30cd7b421b7f24e6512b9d4f91c1ac34a00a7cde
Roman <summedjesters@gmail.com>
2025-04-25 09:21:17 +0500

    fix styles

--- src/main.js
+++ src/main.js
@@ -15,19 +15,20 @@ if (window.Telegram?.WebApp) {
   Telegram.WebApp.expand()
   Telegram.WebApp.enableClosingConfirmation()
 
-  // Устанавливаем цвета из Telegram
-  document.documentElement.style.setProperty(
-    '--tg-theme-bg-color',
-    Telegram.WebApp.backgroundColor
-  )
-  document.documentElement.style.setProperty(
-    '--tg-theme-text-color',
-    Telegram.WebApp.textColor
-  )
-  document.documentElement.style.setProperty(
-    '--tg-theme-button-color',
-    Telegram.WebApp.themeParams.button_color || '#2481cc'
-  )
+  // // Устанавливаем цвета из Telegram
+  // document.documentElement.style.setProperty(
+  //   '--tg-theme-bg-color',
+  //   Telegram.WebApp.themeParams.background_color
+  // )
+  // document.documentElement.style.setProperty(
+  //   '--tg-theme-text-color',
+  //   Telegram.WebApp.themeParams.text_color
+  // )
+  // console.log(Telegram.WebApp.textColor)
+  // document.documentElement.style.setProperty(
+  //   '--tg-theme-button-color',
+  //   Telegram.WebApp.themeParams.button_color || '#2481cc'
+  // )
 }
 
 app.mount('#app')
